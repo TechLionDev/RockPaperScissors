203d1c7ce4b75ca426970c1e9fc972629291a144
TechLionDev <jr.isamuel08.dev@gmail.com>
2022-12-08 16:22:31 -0500

    Finish

--- Game.java
+++ Game.java
@@ -1,12 +1,8 @@
 public class Game {
-    final static String[] moves = {"Rock ✊","Paper ✋","Scissors ✌️"};
-    final static int Rock = 0;
-    final static int Paper = 1;
-    final static int Scissors = 2;
-
-    public static void startPVP() {
-        System.out.println("PVP GAME");
-    }
+    private final static String[] moves = {"Rock ✊","Paper ✋","Scissors ✌️"};
+    private final static int Rock = 0;
+    private final static int Paper = 1;
+    private final static int Scissors = 2;
 
     public static void start(Difficulties level) {
         if (level == Difficulties.EASY){
@@ -14,6 +10,23 @@ public class Game {
             startEasy();
         } else if (level == Difficulties.IMPOSSIBLE) {
             System.out.println("IMPOSSIBLE GAME STARTING...");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("TRAINING AI OPPONENT...");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("AI OPPONENT PRACTICING MOVES...");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             startAI();
         }else{
             try{
@@ -43,7 +56,7 @@ public class Game {
             System.out.print("Would You Like To Play Again? (Y/n): ");
             String again = Main.UserTerminalInput.nextLine();
             if (again.equalsIgnoreCase("y") || again.equalsIgnoreCase("yes")) {
-                Main.Begin();
+                startAI();
             }else{
                 System.exit(0);
             }
@@ -53,7 +66,7 @@ public class Game {
             System.out.print("Would You Like To Play Again? (Y/n): ");
             String again = Main.UserTerminalInput.nextLine();
             if (again.equalsIgnoreCase("y") || again.equalsIgnoreCase("yes")) {
-                Main.Begin();
+                startAI();
             }else{
                 System.exit(0);
             }
@@ -63,6 +76,47 @@ public class Game {
     }
 
     private static void startEasy() {
-
+        int rand = 0 + (int)(Math.random() * ((2 - 0) + 1));
+        String CPChoice;
+        switch (rand) {
+            case 0 -> CPChoice = moves[Rock];
+            case 1 -> CPChoice = moves[Paper];
+            case 2 -> CPChoice = moves[Scissors];
+            default -> CPChoice = null;
+        }
+        System.out.println("Please enter Rock, Paper, or Scissors");
+        String pMove = Main.UserTerminalInput.nextLine();
+        if (pMove.equalsIgnoreCase("Rock")) {
+            if (CPChoice.equalsIgnoreCase(moves[Rock])){
+                System.out.println("You and CP are tied!\n It Chose: " + moves[Rock]);
+            } else if (CPChoice.equalsIgnoreCase(moves[Paper])) {
+                System.out.println("CP Wins!\nIt Chose: " + moves[Paper]);
+            } else if (CPChoice.equalsIgnoreCase(moves[Scissors])){
+                System.out.println("You Win!\nIt Chose: " + moves[Scissors]);
+            }
+        } else if (pMove.equalsIgnoreCase("Paper")) {
+            if (CPChoice.equalsIgnoreCase(moves[Paper])){
+                System.out.println("You and CP are tied!\n It Chose: " + moves[Rock]);
+            } else if (CPChoice.equalsIgnoreCase(moves[Scissors])) {
+                System.out.println("CP Wins!\nIt Chose: " + moves[Scissors]);
+            } else if (CPChoice.equalsIgnoreCase(moves[Rock])){
+                System.out.println("You Win!\nIt Chose: " + moves[Rock]);
+            }
+        } else if (pMove.equalsIgnoreCase("Scissors")) {
+            if (CPChoice.equalsIgnoreCase(moves[Scissors])){
+                System.out.println("You and CP are tied!\n It Chose: " + moves[Scissors]);
+            } else if (CPChoice.equalsIgnoreCase(moves[Rock])) {
+                System.out.println("CP Wins!\nIt Chose: " + moves[Rock]);
+            } else if (CPChoice.equalsIgnoreCase(moves[Paper])){
+                System.out.println("You Win!\nIt Chose: " + moves[Paper]);
+            }
+        }
+        System.out.print("Would You Like To Play Again? (Y/n): ");
+        String again = Main.UserTerminalInput.nextLine();
+        if (again.equalsIgnoreCase("y") || again.equalsIgnoreCase("yes")) {
+            startEasy();
+        }else{
+            System.exit(0);
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -2,24 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static Scanner UserTerminalInput = new Scanner(System.in);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Begin();
     }
 
     public static void Begin() {
         String choice = getGamemode();
-        if (choice.equalsIgnoreCase("PVP")){
-            Game.startPVP();
-        }else if (choice.equalsIgnoreCase("Easy")){
+        if (choice.equalsIgnoreCase("Easy")){
             Game.start(Difficulties.EASY);
         } else if (choice.equalsIgnoreCase("Impossible")) {
             Game.start(Difficulties.IMPOSSIBLE);
+        }else{
+            Begin();
         }
     }
 
     private static String getGamemode() {
         System.out.println("Please Chose One Of The Following Gamemodes:");
-        System.out.println("- PVP");
         System.out.println("- Easy");
         System.out.println("- IMPOSSIBLE");
         String choice = UserTerminalInput.nextLine();
